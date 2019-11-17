fa15153faa9a80e83f5ad349c8396bc91688e944
Phil Simmons <phil@tubbycatgames.com>
2019-11-17 14:36:33 -0500

    Added Win Loss Support tied to the inventory system

--- Assets/Scripts/Manager.cs
+++ Assets/Scripts/Manager.cs
@@ -4,18 +4,9 @@ public class Manager : MonoBehaviour
 {
     public Recipes player1Recipes;
     public Recipes player2Recipes;
-    private Cart cart1;
-    private Cart cart2;
-    // Start is called before the first frame update
-    void Start()
-    {
-        Cart[] carts = FindObjectsOfType<Cart>();
-        foreach (Cart c in carts)
-        {
-            if (c.name == "Cart1") cart1 = c;
-            else if (c.name == "Cart2") cart2 = c;
-        }
-    }
+    public Cart cart1;
+    public Cart cart2;  
+    public WinLoseCanvas winLoseCanvas;
 
     public void GotItem(Cart cart, string itemName)
     {
@@ -25,7 +16,7 @@ public class Manager : MonoBehaviour
             player1Recipes.itemTree.SetItemDone(itemName);
             if (player1Recipes.AreRecipesComplete())
             {
-                print("Player 1 Won");
+                winLoseCanvas.Player1Win();
             }
         }
         else if (cart == cart2 && player2Recipes.itemTree.HasItem(itemName))
@@ -33,7 +24,7 @@ public class Manager : MonoBehaviour
             player2Recipes.itemTree.SetItemDone(itemName);
             if (player2Recipes.AreRecipesComplete())
             {
-                print("Player 2 Won");
+                winLoseCanvas.Player2Win();
             }
         }
         else
@@ -41,12 +32,5 @@ public class Manager : MonoBehaviour
             print("Bad Stuff");
         }
 
-    }
-
-    private void Update()
-    {
-        // if (Input.GetMouseButtonDown(0)) cart1.Lockout();
-        // if (Input.GetMouseButton(1)) cart2.Extend();
-        // else cart2.Retract();
     }
 }
