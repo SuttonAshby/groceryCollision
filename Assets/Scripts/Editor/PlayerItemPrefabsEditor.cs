eff73d7169d2bd5a19a1700925397eda90d37e40
Jake OBrien <neuronrain@gmail.com>
2019-11-17 14:17:16 -0500

    player blocks refs and tool to grab them

---
+++ Assets/Scripts/Editor/PlayerItemPrefabsEditor.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(PlayerItemPrefabs))]
+public class PlayerItemPrefabsEditor : Editor
+{
+
+
+    public override void OnInspectorGUI()
+    {
+        serializedObject.Update();
+        DrawDefaultInspector();
+
+        if (GUILayout.Button("Get Player Items")) AddAllPlayerItemPrefabs();
+        serializedObject.ApplyModifiedProperties();
+    }
+
+
+    private void AddAllPlayerItemPrefabs()
+    {
+        var assetGuids = AssetDatabase.FindAssets("t:prefab", new[] { "Assets/Prefabs/Environment/Blocks" });
+        var foundItems = new List<ObjectID>();
+        foreach (var assetGuid in assetGuids)
+        {
+            var path = AssetDatabase.GUIDToAssetPath(assetGuid);
+            var objectID = (ObjectID)AssetDatabase.LoadAssetAtPath(path, typeof(ObjectID));
+            if (objectID != null) foundItems.Add(objectID);
+        }
+        var spawner = target as PlayerItemPrefabs;
+        spawner.items = foundItems.ToArray();
+    }
+
+}
---
+++ Assets/Scripts/PlayerItemPrefabs.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class PlayerItemPrefabs : ScriptableObject
+{
+
+    public ObjectID[] items;
+
+    public GameObject[] GetPrefabsForIds(string[] ids)
+    {
+        var list = new List<GameObject>();
+        foreach (var id in ids)
+        {
+            foreach (var objectID in items)
+            {
+                if (id == objectID.id)
+                {
+                    list.Add(objectID.gameObject);
+                    break;
+                }
+            }
+        }
+        return list.ToArray();
+    }
+
+}
