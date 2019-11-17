3aa934f806305f77390033d4d7a6dc35b8715781
Nathaniel Trojian <ntrojian@gmail.com>
2019-11-16 23:00:07 -0500

    Add DOTween
    Add DOTween Plugin
    Basic Cart Movement

--- Assets/Scripts/Cart.cs
+++ Assets/Scripts/Cart.cs
@@ -1,16 +1,30 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using DG.Tweening;
 
 public class Cart : MonoBehaviour
 {
     private Manager manager;
+    private bool Moving;
     void Start()
     {
         manager = FindObjectOfType<Manager>();
         if(!manager)
         {
             Debug.LogError("no Manager");
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) Lockout();
+    }
+
+    void Lockout()
+    {
+        if (!Moving)
+        {
+            Moving = true;
+            transform.DOMoveZ(-5, 1).SetLoops(2, LoopType.Yoyo).SetRelative(true).OnComplete(() => Moving = false);
         }
     }
 
