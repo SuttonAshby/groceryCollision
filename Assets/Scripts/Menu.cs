9f79937d455dfbb9c1e705ca336bcf8f73a57933
Nathaniel Trojian <ntrojian@gmail.com>
2019-11-16 20:33:59 -0500

    Add Start Screen
    Moves to Scene Aisle on Click

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    void OnMouseDown()
+    {
+        SceneManager.LoadScene("Aisle", LoadSceneMode.Single);
+    }
+}
