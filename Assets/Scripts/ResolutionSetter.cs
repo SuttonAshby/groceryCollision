877546971f15b771877c21968fc0a74da04797f6
Jake OBrien <neuronrain@gmail.com>
2019-11-18 23:57:34 -0500

    letterboxing ipad. you were right, phil.

---
+++ Assets/Scripts/ResolutionSetter.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResolutionSetter : MonoBehaviour
+{
+
+    public Camera camera;
+
+    private void Start()
+    {
+        #if UNITY_IOS
+        camera.rect = new Rect(0f, 0.1f, 1f, 0.8f);
+        #endif
+    }
+
+}
