75e1fd0b96a8c7a27a61018dc01ee84c3fea41af
Jake OBrien <neuronrain@gmail.com>
2019-11-18 22:14:20 -0500

    PlayerHUD implementation and pooled bling texts

---
+++ Assets/Scripts/AnimatedText.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class AnimatedText : MonoBehaviour
+{
+
+    public Animator animator;
+    public TextMeshProUGUI text;
+
+    public void Play(string message)
+    {
+        text.text = message;
+        gameObject.SetActive(true);
+        animator.SetTrigger("play");
+    }
+
+    public void AnimationComplete()
+    {
+        gameObject.SetActive(false);
+    }
+
+}
--- Assets/Scripts/PlayerHUD.cs
+++ Assets/Scripts/PlayerHUD.cs
@@ -1,28 +1,64 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PlayerHUD : MonoBehaviour
 {
 
+    public AnimatedText gameOverTextPrefab;
+    public AnimatedText collectedItemTextPrefab;
+    public AnimatedText collectedTrashTextPrefab;
+
+    private List<AnimatedText> _collectedItemTexts;
+    private List<AnimatedText> _collectedTrashTexts;
+
     public void PlayerWon()
     {
-
+        var gameOverText = Instantiate(gameOverTextPrefab, transform);
+        gameOverText.Play("YOU WON!");
     }
 
     public void PlayerLost()
     {
-
+        var gameOverText = Instantiate(gameOverTextPrefab, transform);
+        gameOverText.Play("YOU LOST!");
     }
 
     public void CollectedItem(string itemName)
     {
-
+        var text = GetAnimatedText(collectedItemTextPrefab, _collectedItemTexts);
+        text.Play(string.Format("{0}!", itemName));
     }
 
     public void CollectedTrash()
     {
-
+        var text = GetAnimatedText(collectedTrashTextPrefab, _collectedTrashTexts);
+        text.Play("OOPS!");
     }
 
+    private AnimatedText GetAnimatedText(AnimatedText prefab, List<AnimatedText> pool)
+    {
+        if (pool == null) pool = new List<AnimatedText>();
+        if (pool.Count == 0) return CreateAnimatedText(prefab, pool);
+        var existing = GetExistingAnimatedText(prefab, pool);
+        if (existing == null) return CreateAnimatedText(prefab, pool );
+        return existing;
+    }
+
+    private AnimatedText GetExistingAnimatedText(AnimatedText prefab, List<AnimatedText> pool)
+    {
+        foreach (var text in pool)
+        {
+            if (!text.gameObject.activeSelf) return text;
+        }
+        return null;
+    }
+
+    private AnimatedText CreateAnimatedText(AnimatedText prefab, List<AnimatedText> pool)
+    {
+        var text = Instantiate(prefab, transform);
+        pool.Add(text);
+        return text;
+    }
 }
