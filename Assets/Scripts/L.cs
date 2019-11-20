d9d9234f12ae073551e870e7576b95cf54986431
Jake OBrien <neuronrain@gmail.com>
2019-11-20 00:31:39 -0500

    some helpers

---
+++ Assets/Scripts/L.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class L
+{
+
+    public static void og(params System.Object[] objects)
+    {
+        var sb = new System.Text.StringBuilder();
+        foreach (var obj in objects)
+        {
+            sb.AppendFormat("{0} ", obj.ToString());
+        }
+        Debug.Log(sb.ToString());
+    }
+}
