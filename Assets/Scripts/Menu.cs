e21432e262990803f4f13df2e1fd2a6c9e050da3
nate-trojian <ntrojian@gmail.com>
2019-11-21 18:32:52 -0500

    Add QR codes to start screen

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -1,18 +1,37 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
     private AudioSource _audioSource;
+    private bool stages = false;
+    public CanvasGroup carts;
+    public CanvasGroup qrs;
 
     public void Start()
     {
         _audioSource = GetComponent<AudioSource>();
     }
 
-    public void StartGame()
+    public void Next() {
+        if(stages) {
+            StartGame();
+            return;
+        }
+        ShowQRs();
+        stages = true;
+    }
+
+    private void StartGame()
     {
         SceneManager.LoadSceneAsync("Aisle");
         _audioSource.Play();
     }
+
+    public void ShowQRs() {
+        qrs.alpha = 1f;
+        carts.alpha = 0f;
+    }
+
 }
