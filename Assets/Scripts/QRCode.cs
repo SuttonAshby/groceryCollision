0ddfb046e0eb116a3aa8a5781c3219fc24b3c1f4
Phil Simmons <phil@tubbycatgames.com>
2019-11-21 19:38:47 -0500

    Add QR Code support

---
+++ Assets/Scripts/QRCode.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+
+public class QRCode : MonoBehaviour
+{
+    public string Player;
+    void Start()
+    {
+        StartCoroutine(GetTexture());
+    }
+
+    IEnumerator GetTexture() {
+        RawImage i = GetComponent<RawImage>();
+        UnityWebRequest www = UnityWebRequestTexture.GetTexture("https://zxing.org/w/chart?cht=qr&chs=350x350&chld=L&choe=UTF-8&chl=https%3A%2F%2Fshopping-mopup-server.herokuapp.com%2F" + Player);
+        yield return www.SendWebRequest();
+
+        if(www.isNetworkError || www.isHttpError) {
+            Debug.Log(www.error);
+        }
+        else {
+            i.texture = ((DownloadHandlerTexture)www.downloadHandler).texture;
+        }
+    }
+}
